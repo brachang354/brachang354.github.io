54216b335338302f62e9f9e849304fc1536c95bf
Bradley Chang <bradleychang354@gmail.com>
2026-06-09 16:03:52 -0700

    Finished About section, and implemented random joke section

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const createNewElement = function (data) {
+  const { setup, punchline } = data;
+
+  const jokeContainer = document.createElement("div");
+  const jokeSetup = document.createElement("p");
+  const jokePunchline = document.createElement("p");
+
+  jokeSetup.textContent = setup;
+  jokePunchline.textContent = punchline;
+
+  jokeContainer.append(jokeSetup);
+  jokeContainer.append(jokePunchline);
+  return jokeContainer;
+};
+
 const fetchData = async function () {
   const url = "https://official-joke-api.appspot.com/random_joke";
 
@@ -8,6 +23,8 @@ const fetchData = async function () {
     if (response.ok) {
       const data = await response.json();
       console.log(data);
+      const elem = createNewElement(data);
+      joke.append(elem);
     } else {
       throw new Error("Failed to fetch data");
     }
